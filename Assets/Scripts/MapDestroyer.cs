b7de4624eaac8160be74e98bfa0e11c56aae7589
OmerFarukYilmaz-github <omerfarukyilmaz.iletisim@gmail.com>
2023-03-20 17:24:42 +0300

    add bomb and explosion

--- Assets/Scripts/MapDestroyer.cs
+++ Assets/Scripts/MapDestroyer.cs
@@ -9,24 +9,55 @@ public class MapDestroyer : MonoBehaviour
     [SerializeField] Tile wallTile;
     [SerializeField] Tile destructibleTile;
 
+    [SerializeField] GameObject explosionPrefab;
+    [SerializeField] int explosionDistance = 3;
+
     public void Explode(Vector2 worldPos)
     {
         Vector3Int cellPos = tilemap.WorldToCell(worldPos);
         ExplodeCell(cellPos);
+
+        if (ExplodeCell(cellPos + new Vector3Int(1, 0, 0)))
+        {
+            ExplodeCell(cellPos + new Vector3Int(2, 0, 0));
+        }
+
+        if (ExplodeCell(cellPos + new Vector3Int(-1, 0, 0)))
+        {
+            ExplodeCell(cellPos + new Vector3Int(-2, 0, 0));
+        }
+
+        if (ExplodeCell(cellPos + new Vector3Int(0, 1, 0)))
+        {
+            ExplodeCell(cellPos + new Vector3Int(0, 2, 0));
+        }
+
+        if (ExplodeCell(cellPos + new Vector3Int(0, -1, 0)))
+        {
+            ExplodeCell(cellPos + new Vector3Int(0, -2, 0));
+        }
+
     }
 
-    void ExplodeCell(Vector3Int cell)
+    bool ExplodeCell(Vector3Int cell)
     {
+
         Tile tile = (Tile)tilemap.GetTile(cell);
      
+        
+
         if(tile == wallTile)
         {
-            return;
+            return false;
         }
         else if(tile == destructibleTile)
         {
             tilemap.SetTile(cell, null);
         }
+
+        Vector3 pos = tilemap.GetCellCenterWorld(cell);
+        var explosion = Instantiate(explosionPrefab, pos, Quaternion.identity);
+        return true;
     }
 
 }
